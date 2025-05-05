59bb11c8482ba07d7966ca21b251797d88d29f09
mohitsharma1214 <ptms2525@gmail.com>
2025-05-05 23:35:05 +0530

    updated

--- server.js
+++ server.js
@@ -24,8 +24,7 @@ app.use(express.json());
 app.use(cors());
 
 // MongoDB connection
-mongoose
-  .connect('mongodb://localhost:27017/investment', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Connected to MongoDB'))
   .catch((err) => console.error('MongoDB connection error:', err));
 
